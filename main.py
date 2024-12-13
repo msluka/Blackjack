e133562db068e01dfafcb563074d6d7bc8bcab45
Msluka <msluka.info@gmail.com>
2024-12-14 00:23:49 +0100

    Refactor code for precision and fix minor bugs

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def play_blackjack():
     user_cards = random.choices(cards, k=2)
     dealer_cards = random.choices(cards, k=2)
 
+    global play_again
+
     def calculate_scores(cards_list):
         total = sum(cards_list)
         aces = cards_list.count(11)
@@ -23,7 +25,9 @@ def play_blackjack():
         print(f"User cards: {user_cards} Sum: {calculate_scores(user_cards)}")
         print(f"Dealer cards: [{dealer_cards[0]}, ?]\n")
 
-    display_status()
+    def play_again_query():
+        replay_decision = input("Do you want to play again? (y/n): ").lower()
+        return replay_decision == "y"
 
     def user_turn():
         while True:
@@ -34,21 +38,16 @@ def play_blackjack():
                 if user_scores > 21:
                     print("User busted!")
                     print(f"User cards: {user_cards} Sum: {user_scores}\n")
+                    global play_again
+                    play_again = play_again_query()
                     return False
-                elif user_scores == 21:
-                    print("User won!")
-                    print(f"User cards: {user_cards} Sum: {user_scores}\n")
-                    return True
                 else:
                     print(f"User cards: {user_cards} Sum: {user_scores}\n")
             else:
-                return True
-
-    if not user_turn(): # Returns True or False to the caller - Main game loop
-        return True
-
+                return False
 
     def dealer_turn():
+        global play_again
         print(f"Dealer's full cards: {dealer_cards} Sum: {calculate_scores(dealer_cards)}\n")
         while calculate_scores(dealer_cards) < 17:
             dealer_cards.append(new_card())
@@ -59,24 +58,24 @@ def play_blackjack():
 
         if dealer_score > 21 or user_score > dealer_score:
             print("User won!")
+            play_again = play_again_query()
         elif user_score < dealer_score <= 21:
             print("Dealer won!")
+            play_again = play_again_query()
         else:
             print("It's a draw!")
+            play_again = play_again_query()
 
-    dealer_turn()
+    display_status()
 
-    should_continue = input("Do you want to continue? (y/n): ").lower()
-    return should_continue == "y"
-    """
-    if should_continue == "y":
-        return True
+    if calculate_scores(user_cards) == 21:
+        print("*** Blackjack *** User won!")
+        play_again = play_again_query()
     else:
-        return False
-    """
-
+        if not user_turn():
+            dealer_turn()
 
 # Main game loop
 play_again = True
 while play_again:
-    play_again = play_blackjack()
+    play_blackjack()
